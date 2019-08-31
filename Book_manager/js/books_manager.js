8fa194673f24e0a6dc871171696810d6dc83f3f2
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-08-31 16:42:54 +0200

    reverser order of showing books add delete book functionality

--- Book_manager/js/books_manager.js
+++ Book_manager/js/books_manager.js
@@ -44,14 +44,14 @@ function booksSuccess(data) {
         '</thead>' +
         '<tbody>'
     );
-    data.forEach(function (element) {
+    data.reverse().forEach(function (element) {
         let newRow = (
             '<tr data-bookid="' + element['id'] + '" class="text-center clickable">'+
                 '<td>' + element['title'] + '</td>' +
                 '<td>' + element['author'] + '</td>' +
             '</tr>'
         );
-        let emptyRow = $('<div data-bookinfoif="'+ element['id'] +'" class="container justify-content-center d-none book-info">');
+        let emptyRow = $('<div data-bookinfoid="'+ element['id'] +'" class="container justify-content-center d-none book-info">');
         tableBody.append([newRow, emptyRow]);
     tableBody.append('</tbody>')
     });
@@ -96,10 +96,15 @@ function loadBookInfo (id, element) {
                     }
                 })
         }
+    } else {
+        $('.cancel-button').remove();
+        $('.confirm-button').remove();
+        $('.delete-button').removeClass('d-none');
+        $('.edit-button').removeClass('d-none');
     }
 
     function showBookInfoSuccess(data) {
-        let newDiv = $('<table class="table table-hover d-block-flex">');
+        let newDiv = $('<table class="table table-hover mb-0">');
         $.each(data, function (key, val){
             if (key != 'id') {
                 let newRow = (
@@ -112,6 +117,68 @@ function loadBookInfo (id, element) {
             }
         });
         element.append(newDiv);
+        element.append(addDeleteButton());
+        element.append(addEditButton());
     }
+
+    function addEditButton () {
+        let editButton= $('<button class="btn btn-primary mt-0 mb-3 mr-3 edit-button">').text('Edit');
+        editButton.click(function (){
+            editBook()
+        });
+        return editButton
+    }
+
+    function addDeleteButton () {
+        let deleteButton = $('<button class="btn btn-danger mt-0 mb-3 mr-3 delete-button">').text('Delete');
+        deleteButton.click(function (){
+            deleteBook();
+        });
+        return deleteButton
+    }
+
+    function editBook(){
+        console.log('edit me')
+    }
+
+    function deleteBook(){
+        $('.delete-button').toggleClass('d-none');
+        $('.edit-button').toggleClass('d-none');
+        let confirmButton = $('<button class="btn btn-danger mt-0 mb-3 mr-3 confirm-button">').text('Confirm');
+        let cancelButton= $('<button class="btn btn-secondary mt-0 mb-3 mr-3 cancel-button">').text('Cancel');
+        element.append(cancelButton);
+        element.append(confirmButton);
+        cancelButton.click(function (){
+            $('.delete-button').toggleClass('d-none');
+            $('.edit-button').toggleClass('d-none');
+            confirmButton.remove();
+            cancelButton.remove();
+        });
+        confirmButton.click(function () {
+            confirmedDeleteBook(id);
+        })
+    }
+}
+
+function confirmedDeleteBook(id) {
+    $.ajax({
+        url: "http://127.0.0.1:8000/book/" + id,
+        data: {},
+        type: "DELETE",
+        dataType: "json",
+        success: function (data) {
+            console.log('delete book success');
+            $('tr[data-bookid="'+id+'"]').remove();
+            $('div[data-bookinfoid="'+id+'"]').remove();
+        },
+        error: function () {
+            console.log('error @ delete book confirm');
+        },
+        complete: function () {
+            console.log('complete delete book');
+            $('.delete-button').removeClass('d-none');
+            $('.edit-button').removeClass('d-none');
+        }
+    })
 }
 
